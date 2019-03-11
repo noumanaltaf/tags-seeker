8972bfc82f67623d7b77d8be2d1a593e47cc21c1
Nouman Altaf <nouman.altaf@emumba.com>
2019-03-11 13:42:24 +0500

    initial commit

---
+++ example/src/TagsSeeker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ExampleComponent from 'tags-seeker';
+
+export default class TagsSeeker extends Component {
+  render() {
+    return (
+      <div>
+        <ExampleComponent text="Modern React component module" />
+      </div>
+    );
+  }
+}
